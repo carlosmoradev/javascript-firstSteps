9126f15976460f1a4987ce2232b8f152122e4fe3
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-08-05 21:41:22 -0500

    condicional adicional agregado

--- condicionales/pasoPelicula.js
+++ condicionales/pasoPelicula.js
@@ -1,19 +1,21 @@
-const starWars8 = 'The Last Jedi'
+const starWars8 = 'Star Wars 8 - The Last Jedi'
 const pgStarWars8 = 13
 
 const nameCarlos = 'Carlos'
-const ageCarlos = 39
+let ageCarlos = 39
 
 const nameFede = 'Federico'
-const ageFede = 12
+let ageFede = 12
 
-function canWatchStarWars8(name, age) {
-    if (age < pgStarWars8) {
-        alert(`${name} no tiene la edad minima Requerida. su edad es ${age} y la edad minima es ${pgStarWars8}`)
-    } else {
+function canWatchStarWars8(name, age, isWithAdult) {
+    if (age >= pgStarWars8) {
         alert(`${name} Puede pasar a ver ${starWars8}`)
+    } else if (isWithAdult == true) {
+        alert(`${name} puede pasar a ver ${starWars8}.  Aunque su edad es ${age}, se encuentra acompañado por un adulto.`)
+    } else {
+        alert(`${name} no tiene la edad minima Requerida. su edad es ${age} y la edad minima es ${pgStarWars8}`)
     }
 }
 
 canWatchStarWars8(nameCarlos, ageCarlos)
-canWatchStarWars8(nameFede, ageFede)
+canWatchStarWars8(nameFede, ageFede, true)
