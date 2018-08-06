0a81ee73a6761fca170eab0df0663999a6a87b2d
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-08-05 20:35:29 -0500

    comenzando con arrow function

--- calculaTriangulo/triangulo.js
+++ calculaTriangulo/triangulo.js
@@ -6,4 +6,10 @@ function triangleArea(base, height) {
     return base * height / 2
 }
 
-console.log(`el area de un triangulo de base ${base} y altura ${height} es:  ${triangleArea(base, height)}`)
+let triangleAreaArrowFunction = (base, height) => base * height / 2
+
+console.log(`el area de un triangulo de base ${base} y altura ${height} es:  ${triangleArea(base, height)}
+en formato de funcion clasica`)
+
+console.log(`el area de un triangulo de base ${base} y altura ${height} es:  ${triangleAreaArrowFunction(base, height)}
+en formato de funcion flecha o arrow function`)
