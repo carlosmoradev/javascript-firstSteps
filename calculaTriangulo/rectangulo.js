bc69394b0a5932c0a8650a51b80048838776b51e
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-08-05 20:39:45 -0500

    4agregando calculo para rectangulo

---
+++ calculaTriangulo/rectangulo.js
@@ -0,0 +1,9 @@
+// prueba para calcular el area de un rectangulo usando arrow functions
+
+let base = 10
+
+let altura = 30
+
+let calculaRectangulo = (base, altura) => base * altura
+
+console.log(`El área del rectangulo es ${calculaRectangulo(base, altura)}`)
