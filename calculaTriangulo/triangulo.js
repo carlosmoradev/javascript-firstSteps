c442561fe88d15cca8470f4f59790bedef9eed6c
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-08-05 20:29:10 -0500

    creacion basica de funcion

--- calculaTriangulo/triangulo.js
+++ calculaTriangulo/triangulo.js
@@ -1,1 +1,9 @@
-console.log('el area de un triangulo de base 5 y altura 7 es: ' + 5 * 7 / 2)
+let base = 5
+
+let height = 7
+
+function triangleArea(base, height) {
+    return base * height / 2
+}
+
+console.log(`el area de un triangulo de base ${base} y altura ${height} es:  ${triangleArea(base, height)}`)
