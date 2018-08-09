3b51fafdb4f0ce17b862cd3a891e9013d49327b0
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-08-09 07:35:18 -0500

    prototipos validados

--- objetos/calculaDistancia.js
+++ objetos/calculaDistancia.js
@@ -1,23 +1,31 @@
-function Punto(x, y) {
-
+function Punto(x, y) { //este bloque es un constructor
     this.x = x
     this.y = y
-
+        // return this  //el return sobra porque en este caso js lo aplica automaticaente
 }
 
-const p1 = new Punto(0, 4) //new crea un nuevo objeto
-
-
-const p2 = {
-    x: 3,
-    y: 0
+Punto.prototype.moverEnX = function moverEnX(x) {
+    this.x += x
 }
 
-function distancia(p1, p2) {
-    const x = p1.x - p2.x
-    const y = p1.y - p2.x
+Punto.prototype.moverEnY = function moverEnY(y) {
+    this.y += y
+}
+
+Punto.prototype.distancia = function distancia(p) {
+    const x = this.x - p.x
+    const y = this.y - p.y
 
     return Math.sqrt(x * x + y * y)
 }
 
-console.log(distancia(p1, { x: 20, y: -7 }).toFixed(2))
+const p1 = new Punto(0, 4) //new crea un nuevo objeto
+
+const p2 = new Punto(3, 0)
+
+
+console.log(p1.distancia(p2))
+p1.moverEnX(10)
+console.log(p1.distancia(p2))
+p2.moverEnY(-4)
+console.log(p1.distancia(p2))
